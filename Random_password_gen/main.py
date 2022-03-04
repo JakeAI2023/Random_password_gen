0c859a7b27e6bb0535f9c9abf386f940e3451e55
JakeAI2023 <100545618+JakeAI2023@users.noreply.github.com>
2022-03-03 19:47:41 -0500

    Update main.py

--- Random_password_gen/main.py
+++ Random_password_gen/main.py
@@ -7,7 +7,7 @@ lower = "abcdefghijklmnopqrstuvwxyz"
 upper = "ABCDEFGHIJKLMNOPQRSTUVW"
 number = "0123456789"
 symbol = "!@#$%^&*()<>:;?/,~`"
-length = 9 # whatever length you want i chose 9
+length = 9 # whatever length you want i chosed 9
 all = lower + upper + number + symbol
 
 all = "".join(random.sample(all, length))
